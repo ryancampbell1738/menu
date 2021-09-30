5a732ce9b4851393f3412365920c27a873caf557
ryancampbell1738 <90245584+ryancampbell1738@users.noreply.github.com>
2021-09-30 13:02:26 +0100

    Add files via upload

---
+++ menuIdeas/menuIdeas/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace menuIdeas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.pnlMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Controls.Add(this.btnFour);
+            this.pnlMenu.Controls.Add(this.btnThree);
+            this.pnlMenu.Controls.Add(this.btnTwo);
+            this.pnlMenu.Controls.Add(this.btnOne);
+            this.pnlMenu.Location = new System.Drawing.Point(12, 12);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(181, 537);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // btnFour
+            // 
+            this.btnFour.BackColor = System.Drawing.Color.Black;
+            this.btnFour.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnFour.FlatAppearance.BorderSize = 0;
+            this.btnFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.ForeColor = System.Drawing.Color.White;
+            this.btnFour.Location = new System.Drawing.Point(3, 168);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(178, 50);
+            this.btnFour.TabIndex = 0;
+            this.btnFour.Tag = "M";
+            this.btnFour.Text = "Four";
+            this.btnFour.UseVisualStyleBackColor = false;
+            // 
+            // btnThree
+            // 
+            this.btnThree.BackColor = System.Drawing.Color.Black;
+            this.btnThree.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnThree.FlatAppearance.BorderSize = 0;
+            this.btnThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.ForeColor = System.Drawing.Color.White;
+            this.btnThree.Location = new System.Drawing.Point(3, 112);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(178, 50);
+            this.btnThree.TabIndex = 0;
+            this.btnThree.Tag = "M";
+            this.btnThree.Text = "Three";
+            this.btnThree.UseVisualStyleBackColor = false;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.BackColor = System.Drawing.Color.Black;
+            this.btnTwo.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnTwo.FlatAppearance.BorderSize = 0;
+            this.btnTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.ForeColor = System.Drawing.Color.White;
+            this.btnTwo.Location = new System.Drawing.Point(0, 56);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(178, 50);
+            this.btnTwo.TabIndex = 0;
+            this.btnTwo.Tag = "M";
+            this.btnTwo.Text = "Two";
+            this.btnTwo.UseVisualStyleBackColor = false;
+            // 
+            // btnOne
+            // 
+            this.btnOne.BackColor = System.Drawing.Color.Black;
+            this.btnOne.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnOne.FlatAppearance.BorderSize = 0;
+            this.btnOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.ForeColor = System.Drawing.Color.White;
+            this.btnOne.Location = new System.Drawing.Point(0, 0);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(178, 50);
+            this.btnOne.TabIndex = 0;
+            this.btnOne.Tag = "M";
+            this.btnOne.Text = "One";
+            this.btnOne.UseVisualStyleBackColor = false;
+            this.btnOne.Click += new System.EventHandler(this.button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbText);
+            this.panel1.Location = new System.Drawing.Point(210, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(762, 537);
+            this.panel1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(466, 3);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(293, 531);
+            this.rtbText.TabIndex = 0;
+            this.rtbText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox rtbText;
+    }
+}
+
---
+++ menuIdeas/menuIdeas/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menuIdeas
+{
+    public partial class Form1 : Form
+    {
+        int noMenuItems = 0;
+        Button[] mnuItems;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int temp = 0, top = 0;
+
+            pnlMenu.BackColor = Color.Black;
+
+            foreach(Control c in pnlMenu.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    noMenuItems += 1;
+                }
+            }
+
+            mnuItems = new Button[noMenuItems];
+
+            foreach (Control c in pnlMenu.Controls)
+            {
+                if(c.GetType() == typeof(Button))
+                {
+                    mnuItems[temp] = (Button)c;
+
+                    if(c.Tag.Equals("M"))
+                    {
+                        c.BackColor = Color.Black;
+                        c.ForeColor = Color.Gold;
+                        c.Location = new Point(0, top);
+                        top += 50;
+                        c.Visible = true;
+                        c.Tag = c.Tag + "V";
+                    }
+                    else
+                    {
+                        c.BackColor = Color.Gray;
+                        c.ForeColor = Color.Orange;
+                        c.Tag = c.Tag + "I";
+                    }
+                    c.Font = new Font(c.Font, FontStyle.Bold);
+                    c.Tag = c.Tag + temp.ToString();
+                    temp++;
+                }           
+            }
+            rtbText.Font = new Font(FontFamily.GenericSansSerif, 11.0f, FontStyle.Bold);
+            rtbText.Text = "0";
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int startIndex = 0, top = 0;
+            Button btn = (Button)sender;
+
+            rtbText.Font = new Font(FontFamily.GenericSansSerif, 11.0f, FontStyle.Bold);
+
+            startIndex = Convert.ToInt32(btn.Tag.ToString().Substring(2, btn.Tag.ToString().Length - 2));
+
+            if (btn.Tag.ToString().Substring(0,2).Equals("SV"))
+            {
+                switch(startIndex)
+                {
+                    case 1:
+                        rtbText.Text = "1";
+                        break;
+                    case 2:
+                        rtbText.Text = "2";
+                        break;
+                    case 3:
+                        rtbText.Text = "3";
+                        break;
+                    case 4:
+                        rtbText.Text = "4";
+                        break;
+                }
+            }
+            else
+            {
+                rtbText.Font = new Font(FontFamily.GenericSansSerif, 11.0f, FontStyle.Bold);
+                rtbText.Text = "0";
+
+                for(int i = startIndex + 1; i< noMenuItems; 1++)
+                {
+                    if(!mnuItems[i].Tag.ToString().Substring(0,1).Equals("M"))
+                    {
+
+
+                    }
+                }
+            }
+        }
+    }
+}
